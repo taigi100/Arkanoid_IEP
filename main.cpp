6dbb34bdefc3a9ca34f355be39f58836d76d990a
maDaElena24 <46405544+maDaElena24@users.noreply.github.com>
2019-01-06 18:46:39 +0200

    Add collider template

--- main.cpp
+++ main.cpp
@@ -83,6 +83,14 @@ struct Paddle
 
 };
 
+template <class T1, class T2>
+bool isIntersecting(T1& mA, T2& mB)
+{
+    return mA.right() >= mB.left() && mA.left() <= mB.right() &&
+            mA.bottom() >= mB.top() &&
+            mA.top() <= mB.bottom();
+}
+
 int main()
 {
     Ball ball(windowWidth / 2, windowHeight / 2);
