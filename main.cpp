d3f164b280546729a866de922b6c8a5c9f686e02
maDaElena24 <46405544+maDaElena24@users.noreply.github.com>
2019-01-06 18:45:55 +0200

    Add Paddle class

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <SFML/Graphics.hpp>
 
+
 using namespace std;
 using namespace sf;
 
 constexpr int windowWidth(800), windowHeight(600);
 constexpr float ballRadius(10.f), ballVelocity(8.f);
+constexpr float paddleWidth(60.f), paddleHeight(20.f), paddleVelocity(6.f);
 
 struct Ball
 {
@@ -45,10 +47,46 @@ struct Ball
     float bottom() { return y() + shape.getRadius(); }
 };
 
+struct Paddle
+{
+
+    RectangleShape shape;
+    Vector2f velocity;
+
+    Paddle(float mX, float mY){
+        shape.setPosition(mX, mY);
+        shape.setSize(Vector2f(paddleWidth, paddleHeight));
+        shape.setFillColor(Color::Blue);
+        shape.setOrigin(paddleWidth / 2.f, paddleHeight / 2.f);
+
+    }
+
+    void update()
+    {
+        shape.move(velocity);
+
+        if(Keyboard::isKeyPressed(Keyboard::Key::Left) && left() > 0)
+            velocity.x = -paddleVelocity;
+        else if(Keyboard::isKeyPressed(Keyboard::Key::Right) && right() < windowWidth)
+            velocity.x = paddleVelocity;
+        else
+            velocity.x = 0;
+
+    }
+
+    float x() { return shape.getPosition().x; }
+    float y() { return shape.getPosition().y; }
+    float left() { return x() - shape.getSize().x / 2.f; }
+    float right() { return x() + shape.getSize().x / 2.f; }
+    float top() { return y() - shape.getSize().y / 2.f; }
+    float bottom() { return y() + shape.getSize().y / 2.f; }
+
+};
 
 int main()
 {
     Ball ball(windowWidth / 2, windowHeight / 2);
+    Paddle paddle(windowWidth / 2 ,windowHeight - 50);
     RenderWindow window(VideoMode(windowWidth, windowHeight), "Arkanoid");
     window.setFramerateLimit(60);
 
@@ -66,8 +104,10 @@ int main()
         if(Keyboard::isKeyPressed(Keyboard::Key::Escape)) break;
 
         ball.update();
+        paddle.update();
 
         window.draw(ball.shape);
+        window.draw(paddle.shape);
         window.display();
     }
 
