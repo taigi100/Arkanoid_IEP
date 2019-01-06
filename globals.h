4f10f9b980b7f2b3efb93e231086b6ed18f7ea0e
maDaElena24 <46405544+maDaElena24@users.noreply.github.com>
2019-01-06 21:02:57 +0200

    Move globals to header file

---
+++ globals.h
@@ -0,0 +1,5 @@
+constexpr int windowWidth(800), windowHeight(600);
+constexpr float ballRadius(10.f), ballVelocity(5.f);
+constexpr float paddleWidth(100.f), paddleHeight(20.f), paddleVelocity(6.f);
+constexpr float blockWidth(60.f), blockHeight(20.f);
+constexpr int countBlocksX(11), countBlocksY(4);
--- main.cpp
+++ main.cpp
@@ -1,16 +1,8 @@
 #include <SFML/Graphics.hpp>
-
+#include "globals.h"
 
 using namespace std;
 using namespace sf;
-
-constexpr int windowWidth(800), windowHeight(600);
-constexpr float ballRadius(10.f), ballVelocity(5.f);
-constexpr float paddleWidth(60.f), paddleHeight(20.f), paddleVelocity(6.f);
-
-constexpr float blockWidth(60.f), blockHeight(20.f);
-constexpr int countBlocksX(11), countBlocksY(4);
-
 
 struct Ball
 {
