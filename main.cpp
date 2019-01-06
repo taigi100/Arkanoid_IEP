8934a2d42e26e85f49120027dfbee1671d066e55
maDaElena24 <46405544+maDaElena24@users.noreply.github.com>
2019-01-06 20:52:10 +0200

    Add blocks

--- main.cpp
+++ main.cpp
@@ -5,8 +5,12 @@ using namespace std;
 using namespace sf;
 
 constexpr int windowWidth(800), windowHeight(600);
-constexpr float ballRadius(10.f), ballVelocity(8.f);
+constexpr float ballRadius(10.f), ballVelocity(5.f);
 constexpr float paddleWidth(60.f), paddleHeight(20.f), paddleVelocity(6.f);
+
+constexpr float blockWidth(60.f), blockHeight(20.f);
+constexpr int countBlocksX(11), countBlocksY(4);
+
 
 struct Ball
 {
@@ -82,7 +86,27 @@ struct Paddle
     float bottom() { return y() + shape.getSize().y / 2.f; }
 
 };
+struct Brick
+{
+    RectangleShape shape;
+    bool destroyed = false;
 
+    Brick(float mX, float mY)
+    {
+        shape.setPosition(mX, mY);
+        shape.setSize(Vector2f(blockWidth, blockHeight));
+        shape.setFillColor(Color(148,0,211));
+        shape.setOrigin(blockWidth / 2.f, blockHeight / 2.f);
+    }
+
+    float x() { return shape.getPosition().x; }
+    float y() { return shape.getPosition().y; }
+    float left() { return x() - shape.getSize().x / 2.f; }
+    float right() { return x() + shape.getSize().x / 2.f; }
+    float top() { return y() - shape.getSize().y / 2.f; }
+    float bottom() { return y() + shape.getSize().y / 2.f; }
+
+};
 template <class T1, class T2>
 bool isIntersecting(T1& mA, T2& mB)
 {
@@ -102,10 +126,43 @@ void testCollision(Paddle& mPaddle, Ball& mBall)
         mBall.velocity.x = ballVelocity;
 }
 
+void testCollision(Brick& mBrick, Ball& mBall)
+{
+    if(!isIntersecting(mBrick, mBall))
+        return;
+    mBrick.destroyed = true;
+
+     //how much the ball intersects the brick in every direction
+    float overlapLeft(mBall.right() - mBrick.left());
+    float overlapRight(mBrick.right() - mBall.left());
+    float overlapTop(mBall.bottom() - mBrick.top());
+    float overlapBottom(mBrick.bottom() - mBall.top());
+
+    bool ballFromLeft(abs(overlapLeft) < abs(overlapRight));
+    bool ballFromTop(abs(overlapTop) < abs(overlapBottom));
+
+    float minOverlapX  = ballFromLeft ? overlapLeft : overlapRight;
+    float minOverlapY = ballFromTop ? overlapTop : overlapBottom;
+
+    if(abs(minOverlapX) < abs(minOverlapY))
+        mBall.velocity.x = ballFromLeft ? -ballVelocity : ballVelocity;
+    else
+        mBall.velocity.y = ballFromTop ? -ballVelocity : ballVelocity;
+}
+
+
+
 int main()
 {
     Ball ball(windowWidth / 2, windowHeight / 2);
     Paddle paddle(windowWidth / 2 ,windowHeight - 50);
+
+    vector<Brick> bricks;
+
+    for(int iX(0); iX < countBlocksX; ++ iX)
+        for( int iY(0); iY < countBlocksY; ++ iY)
+            bricks.emplace_back((iX + 1) * (blockWidth+3) + 20, (iY +2) *  (blockHeight+3));
+
     RenderWindow window(VideoMode(windowWidth, windowHeight), "Arkanoid");
     window.setFramerateLimit(60);
 
@@ -122,13 +179,24 @@ int main()
         window.clear(Color::Black);
         if(Keyboard::isKeyPressed(Keyboard::Key::Escape)) break;
 
-        testCollision(paddle, ball);
 
         ball.update();
         paddle.update();
+        testCollision(paddle, ball);
+
+        for(auto& brick : bricks) testCollision(brick, ball);
+
+        bricks.erase(remove_if(begin(bricks), end(bricks), [](const Brick& mBrick)
+            {
+                return mBrick.destroyed;
+
+                }),end(bricks));
+
 
         window.draw(ball.shape);
         window.draw(paddle.shape);
+        for(auto& brick : bricks) window.draw(brick.shape);
+
         window.display();
     }
 
